a263a9ec5d1dd5366ca4dd6d522689bbc0cb7642
bluecoolgod80@gmail.com <bluecoolgod80@gmail.com>
2026-01-13 05:34:24 +0900

    [feat] start-node, end-node 구현

---
+++ src/app/api/chat/_nodes/start-node.ts
@@ -0,0 +1,23 @@
+import { type FlowRunnableConfig } from "@/app/api/chat/_constants/runnable-config";
+import { type FlowStateAnnotation } from "@/app/api/chat/flow-state";
+
+export const startNode = async (
+  state: typeof FlowStateAnnotation.State,
+  config: FlowRunnableConfig,
+): Promise<Partial<typeof state>> => {
+  const metadata = config.metadata;
+  const nodeId = metadata?.langgraph_node;
+  if (typeof nodeId !== "string") {
+    throw new Error("nodeId가 문자열이 아닙니다.");
+  }
+
+  const initialInput = state.initialInput;
+  if (typeof initialInput !== "string") {
+    throw new Error("initialInput이 문자열이 아닙니다.");
+  }
+
+  const outputMap = state.outputMap;
+  outputMap[nodeId] = initialInput;
+
+  return { outputMap };
+};
--- src/app/api/chat/flow-state.ts
+++ src/app/api/chat/flow-state.ts
@@ -2,14 +2,19 @@ import { Annotation } from "@langchain/langgraph";
 
 export type OutputMap = Record<string, string | null>;
 export type InputTree = Record<string, Record<string, string>>;
+export type InitialInput = string | null;
 
 export const FlowStateAnnotation = Annotation.Root({
+  initialInput: Annotation<InitialInput>({
+    reducer: (prev, next) => (next === undefined ? prev : next),
+    default: () => null,
+  }),
   outputMap: Annotation<OutputMap>({
-    reducer: (left, right) => ({ ...left, ...right }),
+    reducer: (prev, next) => ({ ...prev, ...next }),
     default: () => ({}),
   }),
   inputTree: Annotation<InputTree>({
-    reducer: (left, right) => ({ ...left, ...right }),
+    reducer: (prev, next) => ({ ...prev, ...next }),
     default: () => ({}),
   }),
 });
